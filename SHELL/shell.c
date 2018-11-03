dd607eeab3d04461158eb1b84f89c60f1e688b79
Rauan Maksatuly <43511161+maksatulyrauan@users.noreply.github.com>
2018-11-03 11:02:43 +0600

    Update shell.c

--- SHELL/shell.c
+++ SHELL/shell.c
@@ -1,1 +1,42 @@
-// your code here
+#include <stdio.h>  
+#include <stdlib.h>
+#include <string.h> 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+int main(int argc, char* argv[]) {
+	char a[20];
+	int y = 0;
+	char o[20] = "exit";
+	char e;
+	while(y != 1) {
+		printf("Enter your command: ");
+		fgets(a, 100, stdin);
+		char c = a[strlen(a) - 2];
+		if(c == '&') {
+			strtok(a, " &");
+			pid_t p = fork();
+			if(p == 0) {
+				char *myargs[2];
+				myargs[0] = strdup(a);
+				myargs[1] = NULL;
+				execvp(myargs[0], myargs);
+				//exit(-1);
+			}
+			else {
+				continue;
+				 
+			}
+		}
+		else {
+			strtok(a, "\n");
+			if(strcmp(a, o) == 0) {
+				break;
+			}
+			system(a);
+		}
+	}
+
+}
