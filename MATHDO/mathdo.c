4e3c8dbda9490fd65839b66795a87d4e67cadc9e
Rauan Maksatuly <43511161+maksatulyrauan@users.noreply.github.com>
2018-11-03 10:58:51 +0600

    Update mathdo.c

--- MATHDO/mathdo.c
+++ MATHDO/mathdo.c
@@ -1,23 +1,69 @@
 #include <stdio.h>
-#include <sys/types.h>
+#include <stdlib.h>  
 #include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
-int main(int argc, char* argv[]){
-	int pid = fork(); // child1
-	if (pid==0){ // child 1
-		execlp("./add.py",":)",argv[1],argv[2],NULL);
+int main(int argc, char* argv[]) {
+	int a = atoi(argv[1]);
+	int b = atoi(argv[2]);
+	pid_t x = fork();
+	if(x == 0) { //child
+		 
+		pid_t y = fork();
+		if(y == 0) {
+			pid_t r = fork();
+			if(r == 0) {
+				char *myargs[5];
+				myargs[0] = strdup("python");
+				myargs[1] = strdup("add.py");
+				myargs[2] = argv[1];
+				myargs[3] = argv[2];
+				myargs[4] = NULL;
+				execvp(myargs[0], myargs);
+			}
+			else {
+				waitpid(r, 0, 0);
+				char *myargs1[5];
+				myargs1[0] = strdup("java");
+				myargs1[1] = strdup("sub");
+				myargs1[2] = argv[1];
+				myargs1[3] = argv[2];
+				myargs1[4] = NULL;
+				execvp(myargs1[0], myargs1);
+			}
+		}
+		else {
+			waitpid(y, 0, 0);
+			char *myargs2[5];
+			myargs2[0] = strdup("js");
+			myargs2[1] = strdup("multi.js");
+			myargs2[2] = argv[1];
+			myargs2[3] = argv[2];
+			myargs2[4] = NULL;
+			execvp(myargs2[0], myargs2);
+		} 
 
-	} else // parent
-	{
-		int pid2 = fork();
-		if (pid2==0){ // child 2
-			execlp("./div.sh",":)",argv[1],argv[2],NULL);
-		} else // parent
-		{
-			wait(0);
-			wait(0);
-			printf("parent: done\n");
-		}		
+	}
+
+	else {
+		waitpid(x, 0, 0);
+		pid_t p = fork();
+		if(p == 0) {
+			char *myargs3[4];
+			myargs3[0] = strdup("./div.sh");
+			myargs3[1] = argv[1];
+			myargs3[2] = argv[2];
+			myargs3[3] = NULL;
+			execvp(myargs3[0], myargs3);
+		}
+		else {
+			waitpid(p, 0, 0);
+			printf("parent: done \n");
+
+		} 
+		 
+
 	}
 }
