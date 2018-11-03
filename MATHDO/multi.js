7b487576366eb38f5129d1ec37c7d9ac3ee97edf
Rauan Maksatuly <43511161+maksatulyrauan@users.noreply.github.com>
2018-11-03 10:59:37 +0600

    Add files via upload

---
+++ MATHDO/multi.js
@@ -0,0 +1,1 @@
+process.stdout.write("nodejs: " + process.argv[2] + "*" + process.argv[3] + "=" + (parseInt(process.argv[2]) * parseInt(process.argv[3])) + "\n");
---
+++ MATHDO/sub.java
@@ -0,0 +1,7 @@
+class sub {
+	public static void main(String[]args) {
+		int a = Integer.parseInt(args[0]);	
+		int b = Integer.parseInt(args[1]);
+		System.out.printf("java: %d-%d=%d%n", a, b, (a - b));
+	}
+}
